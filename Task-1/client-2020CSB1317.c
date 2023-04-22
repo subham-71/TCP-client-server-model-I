e9eaa91ca1784603fd6c3db7e3a2eecca10005fd
subham <subhamsubhasis2002@gmail.com>
2023-04-22 19:02:40 +0530

    Task-1

---
+++ Task-1/client-2020CSB1317.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 5555
+#define IP "127.0.0.1"
+
+void client_socket_init(int *client_socket)
+{
+    // Create a client socket
+    *client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (*client_socket == -1)
+    {
+        printf("Error: Failed to create socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+void server_connection(int *client_socket, struct sockaddr_in *server_address)
+{
+    // Connect to the server
+    server_address->sin_family = AF_INET;
+    server_address->sin_addr.s_addr = inet_addr(IP); // Replace with the IP address of your server
+    server_address->sin_port = htons(PORT);
+
+    if (connect(*client_socket, (struct sockaddr *)server_address, sizeof(*server_address)) == -1)
+    {
+        printf("Error: Failed to connect to server \n");
+    }
+    else {
+        printf("Connected to server \n");
+    }
+}
+
+int main()
+{
+    int client_socket;
+    struct sockaddr_in server_address;
+    char buffer[1024];
+
+    client_socket_init(&client_socket);
+
+    server_connection(&client_socket, &server_address);
+   
+    // Send a string message to the server
+    printf("Enter a string to send to the server \n ");
+    fgets(buffer, sizeof(buffer), stdin);
+    send(client_socket, buffer, strlen(buffer), 0);
+
+    // Receive the reversed message from the server
+    int read_size = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (read_size == -1)
+    {
+        printf("Error: Failed to receive message from server\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Print the reversed message on the console
+    printf("Reversed string received from server: %.*s \n \n ", read_size, buffer);
+
+    // Close client socket
+    close(client_socket);
+
+    return 0;
+}
---
+++ Task-1/server-2020CSB1317.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 5555
+
+void handle_client(int client_socket)
+{
+    char buffer[1024];
+    int read_size;
+
+    printf("[NEW CONNECTION] Client connected\n");
+
+    while ((read_size = recv(client_socket, buffer, sizeof(buffer), 0)) > 0)
+    {
+        // Reverse the string
+        for (int i = 0; i < read_size / 2; i++)
+        {
+            char temp = buffer[i];
+            buffer[i] = buffer[read_size - i - 1];
+            buffer[read_size - i - 1] = temp;
+        }
+
+        // Send the reversed string back to the client
+        send(client_socket, buffer, read_size, 0);
+    }
+
+    // Close the client socket
+    close(client_socket);
+
+    printf("[DISCONNECTED] Client disconnected\n");
+}
+
+int main()
+{
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    int address_length = sizeof(server_address);
+
+    // Create a server socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
+    {
+        printf("Error: Failed to create socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the server socket to the port
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        printf("Error: Failed to bind socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming client connections
+    if (listen(server_socket, 5) == -1)
+    {
+        printf("Error: Failed to listen for connections\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[LISTENING] Server is listening on port %d\n", PORT);
+
+    while (1)
+    {
+        // Accept an incoming client connection
+        client_socket = accept(server_socket, (struct sockaddr *)&client_address, (socklen_t *)&address_length);
+        if (client_socket == -1)
+        {
+            printf("Error: Failed to accept connection\n");
+            continue;
+        }
+
+        // Create a new thread to handle the client connection
+        if (fork() == 0)
+        {
+            // This is the child process, handle the client connection
+            close(server_socket);
+            handle_client(client_socket);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            // This is the parent process, close the client socket and continue listening for connections
+            close(client_socket);
+        }
+    }
+
+    return 0;
+}
